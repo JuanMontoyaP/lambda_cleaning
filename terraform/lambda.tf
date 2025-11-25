b26d9aa9a5c0ff065df855918f43f0d5b642a01b
Juan P Montoya <84058843+JuanMontoyaP@users.noreply.github.com>
2025-11-25 11:30:14 -0500

    Fix policies

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -6,10 +6,16 @@ resource "aws_lambda_function" "cleaner" {
   runtime          = "python3.13"
   role             = aws_iam_role.lambda_role.arn
 
+  reserved_concurrent_executions = 5
+
   environment {
     variables = {
       SNS_TOPIC_ARN = aws_sns_topic.runtime_alerts.arn
     }
+  }
+
+  tracing_config {
+    mode = "Active"
   }
 
   depends_on = [aws_cloudwatch_log_group.lambda_logs]
